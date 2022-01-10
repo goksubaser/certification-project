71bca7de5230cd9fe0e86493e5fcab3c0efd4b80
Göksu Başer <goksubaser@gmail.com>
2022-01-10 20:09:22 +0300

    GRADUATED_ROLE added

--- contracts/Roles.sol
+++ contracts/Roles.sol
@@ -8,4 +8,5 @@ contract Roles is AccessControl {
     bytes32 public constant DEPARTMENT_ROLE = keccak256("DEPARTMENT_ROLE");
     bytes32 public constant INSTRUCTOR_ROLE = keccak256("INSTRUCTOR_ROLE");
     bytes32 public constant STUDENT_ROLE = keccak256("STUDENT_ROLE");
+    bytes32 public constant GRADUATED_ROLE = keccak256("GRADUATED_ROLE");
 }
